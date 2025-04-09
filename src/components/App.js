c35665c8d4647c4ceb8e6c0b2b7cdda919b3bb79
LAETITIA-hub <laetitia.kamangu@student.moringaschool.com>
2025-04-09 13:25:13 +0300

    add all changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
-import TestComponent from "./TestComponent"; // Added this import
+import TestComponent from "./TestComponent"; 
 
-// Add your code own within the return statement
 function App() {
   return (
     <div className="App">
@@ -15,7 +14,7 @@ function App() {
         self-contained chunks of JSX.
       </p>
       <ExampleComponent />
-      <TestComponent /> {/* Now this is valid */}
+      <TestComponent /> 
     </div>
   );
 }
